06ebd39a2b35e2fa6061ff78a46052d20b3375de
Mariusz543534233 <76970415+Mariusz543534233@users.noreply.github.com>
2021-02-22 13:39:15 +0100

    1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import *
+
 napis = 'to jest napis 2+2'
 a = (6-2)
 print (napis)
@@ -9,3 +11,17 @@ b, c, d = 1, 2, 3
 print(b)
 print(c)
 print(d)
+
+dzielenie =b/c
+print(dzielenie)
+dzielenie_calkowite = b // c
+print(dzielenie_calkowite)
+
+dodawanie =a +b
+potega = a**c
+print (dodawanie)
+print (potega)
+print ("math")
+potega = pow(b, c)
+print(potega)
+print(pi)
